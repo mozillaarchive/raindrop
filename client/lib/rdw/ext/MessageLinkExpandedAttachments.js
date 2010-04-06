14deb9d92d3d5f62812491048fdc1fc745c16b1b
Bryan Clark <clarkbw@gnome.org>
2010-04-06 02:08:04 -0700

    convert the bit.ly expander into a more generic bitly api service expander.  add the su.pr link service.  converge expanded links to the generic schema rd.msg.body.attachment.link.expanded and move the bit.ly ui extension over to a more generic version.

---
+++ client/lib/rdw/ext/MessageLinkExpandedAttachments.js
@@ -0,0 +1,95 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Raindrop.
+ *
+ * The Initial Developer of the Original Code is
+ * the Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2010
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * */
+
+/*jslint plusplus: false, nomen: false */
+/*global require: false */
+"use strict";
+
+require.modify("rdw/Conversations", "rdw/ext/MessageLinkExpandedAttachments-rdw/Conversations",
+    ["rd", "dojo", "rd/api", "rdw/Conversations"],
+    function (rd, dojo, api) {
+        rd.applyExtension("rdw/ext/MessageLinkExpandedAttachments", "rdw/Conversations", {
+            addToPrototype: {
+                personalSchemas: [
+                    "rd.msg.body.attachment.link.expanded"
+                ]
+            }
+        });
+    }
+);
+
+require.modify("rdw/Message", "rdw/ext/MessageLinkExpandedAttachments",
+["require", "rd", "dojo", "rd/schema", "rdw/Message"], function (
+  require,   rd,   dojo,   rdSchema,    Message) {
+    /*
+    Applies a display extension to rdw/Message.
+    Allows showing links included in the message as inline attachments
+    */
+
+    rd.addStyle("rdw/ext/css/MessageLinkExpandedAttachments");
+
+    rd.applyExtension("rdw/ext/MessageLinkExpandedAttachments", "rdw/Message", {
+        addToPrototype: {
+            linkHandlers: [
+                function (link) {
+                    //NOTE: the "this" in this function is the instance of rdw/Message.
+                    var schema = rdSchema.getMsgMultipleMatch(this.msg, "rd.msg.body.attachment.link.expanded", "ref_link", link.url),
+                          linkNode, templateObj, template, titleTemplate;
+                    if (!schema) {
+                        return false;
+                    }
+
+                    template = '<a target="_blank" class="title long_url" title="${long_url}" href="${short_url}">${long_url}</a>' +
+                               '<div class="description">${description}</div>' +
+                               '<span class="by">by</span> ' +
+                               '<abbr class="owner" title="${user_name}@${domain}">${display_name}</abbr>';
+
+                    titleTemplate = '<a target="_blank" class="title" title="${long_url}" href="${short_url}">${title}</a>' +
+                                    '<div class="description">${description}</div>' +
+                                    '<span class="by">by</span> ' +
+                                    '<abbr class="owner" title="${user_name}@${domain}">${display_name}</abbr>';
+
+                    templateObj = {
+                        long_url      : schema.long_url,
+                        short_url     : schema.short_url,
+                        domain        : schema.domain,
+                        title         : schema.title || "",
+                        description   : schema.description || "",
+                        display_name  : schema.display_name || schema.user_name,
+                        user_name     : schema.user_name || ""
+                    };
+
+                    if (schema.title) {
+                        template = titleTemplate;
+                    }
+
+                    this.addAttachment('<div domain="' + schema.domain +
+                                       '" class="link expanded">' +
+                                       rd.template(template, templateObj) +
+                                       '</div>', 'link');
+                    return true;
+                }
+            ]
+        }
+    });
+});
---
+++ couch_docs/wq/rd.ext.core.hyperlinks.bitly-api-shortener.py
@@ -0,0 +1,172 @@
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is Raindrop.
+#
+# The Initial Developer of the Original Code is
+# Mozilla Messaging, Inc..
+# Portions created by the Initial Developer are Copyright (C) 2009
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#
+
+import re
+import urllib2, json
+
+SERVICES = {
+  "su.pr" : {
+   'link_prop'            : 'path',
+   'regex'                : re.compile('^/(\w+)'),
+   'api'                  : "http://su.pr/api/expand",
+   'hash_option_function' : lambda link, hash: link,
+   'hash_option_name'     : 'shortUrl',
+   'options' : { },
+   'schema' : {'short_url'     : lambda link, schema: link.get('url'),
+               'long_url'      : lambda link, schema: schema.get('longUrl'),
+               'title'         : lambda link, schema: schema.get('longUrl'),
+               'thumbnail'     : lambda link, schema: None,
+               'user_name'     : lambda link, schema: link.get('domain'),
+               'display_name'  : lambda link, schema: None,
+               'user_url'      : lambda link, schema: None,
+               'description'   : lambda link, schema: None,
+               }
+   },
+  "bit.ly" : {
+   'link_prop'            : 'path',
+   'regex'                : re.compile('^/(\w+)'),
+   'api'                  : "http://api.bit.ly/info",
+   'hash_option_function' : lambda link, hash: hash,
+   'hash_option_name'     : 'hash',
+   'options' : {'version' : '2.0.1',
+                'login'   : 'bitlyapidemo', # demo API user
+                'apiKey'  : 'R_0da49e0a9118ff35f52f629d2d71bf07', # demo API key
+                },
+    'schema' : {'short_url'     : lambda link, schema: link.get('url'),
+                'long_url'      : lambda link, schema: schema.get('longUrl'),
+                'title'         : lambda link, schema: schema.get('htmlTitle'),
+                'thumbnail'     : lambda link, schema: schema.get('thumbnail', { 'small' : None }).get('small'),
+                'user_name'     : lambda link, schema: schema.get('shortenedByUser'),
+                'display_name'  : lambda link, schema: schema.get('shortenedByUser'),
+                'user_url'      : lambda link, schema: "http://%s/user/recent/%s" % (link.get('domain'), schema.get('shortenedByUser')),
+                'description'   : lambda link, schema: schema.get('htmlMetaDescription')
+                }
+  },
+  "bitly.com" : {
+   'link_prop'            : 'path',
+   'regex'                : re.compile('^/(\w+)'),
+   'api'                  : "http://api.bit.ly/info",
+   'hash_option_function' : lambda link, hash: hash,
+   'hash_option_name'     : 'hash',
+   'options' : {'version' : '2.0.1',
+                'login'   : 'bitlyapidemo', # demo API user
+                'apiKey'  : 'R_0da49e0a9118ff35f52f629d2d71bf07', # demo API key
+                },
+    'schema' : {'short_url'     : lambda link, schema: link.get('url'),
+                'long_url'      : lambda link, schema: schema.get('longUrl'),
+                'title'         : lambda link, schema: schema.get('htmlTitle'),
+                'thumbnail'     : lambda link, schema: schema.get('thumbnail', { 'small' : None }).get('small'),
+                'user_name'     : lambda link, schema: schema.get('shortenedByUser'),
+                'display_name'  : lambda link, schema: schema.get('shortenedByUser'),
+                'user_url'      : lambda link, schema: "http://%s/user/recent/%s" % (link.get('domain'), schema.get('shortenedByUser')),
+                'description'   : lambda link, schema: schema.get('htmlMetaDescription')
+                }
+  },
+  "j.mp" : {
+   'link_prop'            : 'path',
+   'regex'                : re.compile('^/(\w+)'),
+   'api'                  : "http://api.bit.ly/info",
+   'hash_option_function' : lambda link, hash: hash,
+   'hash_option_name'     : 'hash',
+   'options' : {'version' : '2.0.1',
+                'login'   : 'bitlyapidemo', # demo API user
+                'apiKey'  : 'R_0da49e0a9118ff35f52f629d2d71bf07', # demo API key
+                },
+    'schema' : {'short_url'     : lambda link, schema: link.get('url'),
+                'long_url'      : lambda link, schema: schema.get('longUrl'),
+                'title'         : lambda link, schema: schema.get('htmlTitle'),
+                'thumbnail'     : lambda link, schema: schema.get('thumbnail', { 'small' : None }).get('small'),
+                'user_name'     : lambda link, schema: schema.get('shortenedByUser'),
+                'display_name'  : lambda link, schema: schema.get('shortenedByUser'),
+                'user_url'      : lambda link, schema: "http://%s/user/recent/%s" % (link.get('domain'), schema.get('shortenedByUser')),
+                'description'   : lambda link, schema: schema.get('htmlMetaDescription')
+                }
+  },
+  "nyti.ms" : {
+   'link_prop'            : 'path',
+   'regex'                : re.compile('^/(\w+)'),
+   'api'                  : "http://api.bit.ly/info",
+   'hash_option_function' : lambda link, hash: hash,
+   'hash_option_name'     : 'hash',
+   'options' : {'version' : '2.0.1',
+                'login'   : 'bitlyapidemo', # demo API user
+                'apiKey'  : 'R_0da49e0a9118ff35f52f629d2d71bf07', # demo API key
+                },
+    'schema' : {'short_url'     : lambda link, schema: link.get('url'),
+                'long_url'      : lambda link, schema: schema.get('longUrl'),
+                'title'         : lambda link, schema: schema.get('htmlTitle'),
+                'thumbnail'     : lambda link, schema: schema.get('thumbnail', { 'small' : None }).get('small'),
+                'user_name'     : lambda link, schema: schema.get('shortenedByUser'),
+                'display_name'  : lambda link, schema: schema.get('shortenedByUser'),
+                'user_url'      : lambda link, schema: None,
+                'description'   : lambda link, schema: schema.get('htmlMetaDescription')
+                }
+  },
+}
+
+def handler(doc):
+    if not 'links' in doc:
+        return
+
+    shorties = []
+    for link in doc['links']:
+        service = SERVICES.get(link['domain'], None)
+        if service is not None:
+            prop = service.get('link_prop')
+            match = service.get('regex').search(link[prop])
+            if match and match.group(1):
+                shorties.append( (link, match.group(1)) )
+
+    if len(shorties) == 0:
+        return
+
+    for link, hash in shorties:
+        service = SERVICES.get(link['domain'])
+
+        options = service.get('options')
+        options[service.get('hash_option_name')] = service.get('hash_option_function')(link['url'],hash)
+
+        api = "%s?%s" % (service.get('api'), "&".join(['%s=%s' % v for v in options.items()]))
+
+        opener = urllib2.build_opener()
+        obj = json.load(opener.open(api))
+        if obj.get('errorCode') == 0:
+            shorty = obj.get('results').get(hash)
+            ss = service.get("schema")
+            # XXX not all of these items are actually used, we could trim down
+            # the size of these documents if space needed to be saved but for
+            # now it's nice to have the extra data in case we want it later
+            schema = {"short_url"    : ss.get("short_url")(link, shorty),
+                      "long_url"     : ss.get("long_url")(link, shorty),
+                      "title"        : ss.get("title")(link, shorty),
+                      "thumbnail"    : ss.get("thumbnail")(link, shorty),
+                      "user_name"    : ss.get("user_name")(link, shorty),
+                      "display_name" : ss.get("display_name")(link, shorty),
+                      "user_url"     : ss.get("user_url")(link, shorty),
+                      "description"  : ss.get("description")(link, shorty),
+                      "extra"        : shorty,
+                      "is_attachment": True,
+                      "domain"       : link.get('domain'),
+                      "ref_link"     : link['url']
+                      }
+            emit_schema('rd.msg.body.attachment.link.expanded', schema)
