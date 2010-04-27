40d8e5ba71526220a5ed27f8dad13f8663c3ca09
Mark Hammond <mhammond@skippinet.com.au>
2010-04-27 14:52:26 +1000

    fix field name for mailing-list view
    --HG--
    extra : rebase_source : 381b849812388dd02fd0f84e5351de7c80578fb9

--- schema/content/all/msg_by_mailinglist-map.js
+++ schema/content/all/msg_by_mailinglist-map.js
@@ -1,4 +1,4 @@
 function(doc) {
-    if (doc.rd_schema_id == "rd.msg.email.mailing-list" && doc.listid)
-        emit(doc.listid, {rd_key: doc.rd_key, _rev: doc._rev});
+    if (doc.rd_schema_id == "rd.msg.email.mailing-list" && doc.list_id)
+        emit(doc.list_id, {rd_key: doc.rd_key, _rev: doc._rev});
 }
